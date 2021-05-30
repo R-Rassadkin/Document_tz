ddb63b94d2d4f45307fab163c75a4016d323b6ea
R-Rassadkin <rassadkin0201@gmail.com>
2021-05-30 21:11:58 +0300

    document tz

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <document
+      :name="name">
+      </document>
+  </div>
+</template>
+
+<script>
+import document from './components/document'
+
+export default {
+  name: 'App',
+   data(){
+     return{
+       name:'FIO'
+     }
+   },
+   components:{
+     document
+   }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
